ef0cf63d91bb40ab3c61259a05db6ebbc2213cf8
Ayush Jadaun <jadaunayush26@gmail.com>
2026-06-05 19:07:27 -0700

    Implement and link login and register pages

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React,{useState} from 'react'
+import { useNavigate, Link } from 'react-router'
+import "../auth.form.scss"
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  );
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
 }
 
-export default Login;
+
+
+    return (
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email" id="email" name='email' placeholder='Enter email address'     
+                            autoComplete="off" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name='password' placeholder='Enter password'   
+                            autoComplete="new-password"/>
+                    </div>
+                    <button className='button primary-button' >Login</button>
+                </form>
+                <p>Don't have an account? <Link to={"/register"} >Register</Link> </p>
+            </div>
+        </main>
+    )
+}
+
+export default Login
